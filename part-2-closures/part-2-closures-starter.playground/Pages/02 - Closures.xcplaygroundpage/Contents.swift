f3bf199ef4ecedd152d2568d530731883ced1cdd
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-04-20 12:19:42 -0400

    lesson completed

--- part-2-closures/part-2-closures-starter.playground/Pages/02 - Closures.xcplaygroundpage/Contents.swift
+++ part-2-closures/part-2-closures-starter.playground/Pages/02 - Closures.xcplaygroundpage/Contents.swift
@@ -14,8 +14,30 @@ func printResult(_ operate: Operate, _ a: Int, _ b: Int) {
 }
 // --------------------------------------
 
+var operate = add
+//var addClosure: (Int, Int) -> Int
+//var addClosure: Operate = { (a: Int, b: Int) -> Int in
+//  return a + b
+//}
+var addClosure = { (a: Int, b: Int) -> Int in
+ a + b
+}
 
 
 
+add(a: 7, b: 3)
+operate(7, 3)
+addClosure(7, 3)
+
+printResult(add, 4, 5)
+printResult(operate, 4, 5)
+printResult(add, 4, 5)
+printResult(+, 4, 5)
+
+// Define an inline closure
+printResult( { (a: Int, b: Int) -> Int in
+  a * b + 100
+  },
+3, 10)
 
 //: [⇒ Next: 03 - Challenge - Closures](@next)
