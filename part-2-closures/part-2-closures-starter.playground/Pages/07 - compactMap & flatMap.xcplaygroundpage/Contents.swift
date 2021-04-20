f02ab63bc186790f2f8b403e9b5ef85a32e4390f
Philippe Timothe <PhilippeTimothe@gmail.com>
2021-04-20 14:46:55 -0400

    compact and flat map lesson done

--- part-2-closures/part-2-closures-starter.playground/Pages/07 - compactMap & flatMap.xcplaygroundpage/Contents.swift
+++ part-2-closures/part-2-closures-starter.playground/Pages/07 - compactMap & flatMap.xcplaygroundpage/Contents.swift
@@ -2,18 +2,29 @@
 //: ## Episode 07: compactMap & flatMap
 
 //: `compactMap`
-
+// Compact Map helps deal with optionals or when a collection of data might produce optionals
 // --------------------------------------
 let userInput = ["meow!", "15", "37.5", "seven"]
 // --------------------------------------
 
+var arrayForInput: [Int] = []
+for input in userInput {
+  //  safe unrapping because not all array elements can be converted to an integer
+  guard let input = Int(input) else {
+    continue
+  }
+  arrayForInput.append(input)
+}
 
+arrayForInput
 
-
-
+//compact map example
+let validInput = userInput.compactMap { (input) in
+  Int(input)
+}
 
 //: `flatMap`
-
+// Helps handle mutildemensional arrays
 // --------------------------------------
 let arrayOfDwarfArrays = [
   ["Sleepy", "Grumpy", "Doc", "Bashful", "Sneezy"],
@@ -21,6 +32,12 @@ let arrayOfDwarfArrays = [
 ]
 // --------------------------------------
 
-
+let dwarvesAfterM = arrayOfDwarfArrays.flatMap { (dwarves) -> [String] in
+  var afterM: [String] = []
+  for dwarf in dwarves where dwarf > "M" {
+    afterM.append(dwarf)
+  }
+  return afterM
+}
 
 //: [⇒ Next: 08 - Challenge: Closures & Collections](@next)
